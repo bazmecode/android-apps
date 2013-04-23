e1c7fc461cb7bc5c531e25eb9dc3fa1d0b6c348e
bazmecode <azmi_wasif@yahoo.com>
2013-04-23 23:06:04 +0530

    Connected new message,inbox & sent messages activites with the MainActivity

--- SecureMessaging/src/com/example/securemessaging/MainActivity.java
+++ SecureMessaging/src/com/example/securemessaging/MainActivity.java
@@ -1,15 +1,55 @@
 package com.example.securemessaging;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
+	Button newMsgBtn;
+	Button inboxBtn;
+	Button sentMsgsBtn;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		addButtonClickListeners();
+	}
+
+	private void addButtonClickListeners() {
+		final Context context = this;
+
+		newMsgBtn = (Button) findViewById(R.id.newMsgBtn);
+		newMsgBtn.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View arg0) {
+				Intent intent = new Intent(context, NewMessageActivity.class);
+				startActivity(intent);
+			}
+		});
+
+		inboxBtn = (Button) findViewById(R.id.inboxBtn);
+		inboxBtn.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View arg0) {
+				Intent intent = new Intent(context, InboxActivity.class);
+				startActivity(intent);
+			}
+		});
+
+		sentMsgsBtn = (Button) findViewById(R.id.sentMsgsBtn);
+		sentMsgsBtn.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View arg0) {
+				Intent intent = new Intent(context, SentMessagesActivity.class);
+				startActivity(intent);
+			}
+		});
 	}
 
 	@Override
